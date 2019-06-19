cdc755858e44d8f99b736de1a59c0d20fae752ae
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2019-06-19 12:21:25 -0400

    Update cleanname.py

--- cleanname.py
+++ cleanname.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from nameparser import HumanName
 import string
+from titlecase import titlecase
 
 xls = pd.ExcelFile('xxxxxxx.xls')
 df = xls.parse('SurveysAnswersCrosstab', skiprows=4, index_col=None) #skip first 4 rows
@@ -13,6 +14,9 @@ df = df.drop(df.index[0]) #drop the first row
 df = df.drop_duplicates("ID", keep='first', inplace=False) #drop duplicate records by "ID"
 df = df.reset_index(drop=True)
 df = df.fillna(' ') #fill all the NaN with ' '
+for i in range(0,len(df['Title'])): #Clean data in column 'Title'
+    df['Title'][i] = df['Title'][i].replace(".", " ")
+    df['Title'][i] = df['Title'][i].replace("  ", " ")
 
 list1 = []
 list2 = []
@@ -35,8 +39,7 @@ col = ['First Name', 'Title', 'Company', 'Address', 'State', 'City', 'Country']
 def capwordallnew(dataset):
     for item in col:
         for i in range(0, len(dataset)):
-            if str(dataset[item][i]) != 'United States of America':
-                dataset[item][i] = string.capwords(dataset[item][i])
+            dataset[item][i] = titlecase(dataset[item][i])
     return dataset
 
 #Capitalize last name with Mc, Mac...
@@ -48,18 +51,28 @@ def caplastname(dataset):
         ln = name.first
         dataset['Last Name'][i] = ln
     return dataset
-    
+
+#Clean datasets by functions.
 n1 = capwordallnew(n1)
 n1 = caplastname(n1)
 n2 = capwordallnew(n2)
 n2 = caplastname(n2)
 
+#Delete the suffix in the string of column 'Company'
+comsuffix = [' Inc', ', Inc', ' Llp', ', Llp', ' As',', As',' Llc', ', Llc',' Ltd', ', Ltd', ',']
+for item in comsuffix:
+    n1['Company'] = n1['Company'].str.split(item).str[0]
+for item in comsuffix:
+    n2['Company'] = n2['Company'].str.split(item).str[0]
+    
 #Delete the rows depends on whether the selected column contains a specific parameter.
 def dropbycol(col,para):
-    new = n2[~n2[col].str.contains(para)]
+    new = n2[~n2[col].str.contains(para,na=False)]
     return new
     
 #for example, delete all the rows depends on whether the 'e-Mail' column contains 'edu'
 n2 = dropbycol('e-Mail','edu') 
+
+#Save to excel
 n1.to_excel('n1.xlsx',header=True, index=False)
 n2.to_excel('n2.xlsx',header=True, index=False)
