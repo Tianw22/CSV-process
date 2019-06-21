3a35dc2214711261b52a7accd681713aa7c2546a
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2019-06-21 16:01:34 -0400

    Update spacer.py

--- spacer.py
+++ spacer.py
@@ -1,8 +1,8 @@
 #Add(insert) space between letters and numbers
 #在数字和字母间加空格
 import re
-txt = '!!!this, 1is, 2a 3test!!!'
+txt = 'this, 1is, 2a 3test'
 def spacer(text):
     return re.sub(r'([0-9])([a-zA-Z])',r"\1 \2",text,re.MULTILINE).strip()
 print(spacer(txt))
-#Output => !!!this, 1 is, 2 a 3 test!!!
+#Output => this, 1 is, 2 a 3 test
